d009c4d1843f526b91b3ba60e794c47ebd5b4b01
Pavel <Klopat>
2023-08-24 15:31:08 +0300

    project common files and setup

---
+++ RickAndMorty/RickAndMorty/Extensions/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift
+//  RickAndMorty
+//
+//  Created by Паша Клопот on 21.08.23.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    var topBarHeight: CGFloat {
+        var top = self.navigationController?.navigationBar.frame.height ?? 0.0
+        if #available(iOS 13.0, *) {
+            top += UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        } else {
+            top += UIApplication.shared.statusBarFrame.height
+        }
+        return top
+    }
+}
