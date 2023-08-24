f431fb3a396ef02c1a972074f8d7746889d078a7
Pavel <Klopat>
2023-08-24 18:36:00 +0300

    refactored and updated character model and viewModel

---
+++ RickAndMorty/RickAndMorty/CharactersList/Models/CharacterInfoModel.swift
@@ -0,0 +1,54 @@
+//
+//  CharacterInfoModel.swift
+//  RickAndMorty
+//
+//  Created by Паша Клопот on 21.08.23.
+//
+
+import Foundation
+
+// MARK: - AllCharactersInfoModel
+struct AllCharactersInfoModel: Codable {
+    var results: [CharacterInfoModel]?
+}
+
+// MARK: - Result
+struct CharacterInfoModel: Codable {
+    var id: Int
+    var name: String
+    var aliveStatus: String
+    var creatureType: String
+    let type: String
+    var gender: String
+    var origin: LocationModel
+    var location: LocationModel
+    var image: String
+    var episode: [String]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name, type, gender, origin, location, image, episode
+        case aliveStatus = "status"
+        case creatureType = "species"
+    }
+}
+
+// MARK: - Location
+struct LocationModel: Codable {
+    var name: String?
+    var url: String?
+}
+
+// MARK: - Welcome
+struct EpisodeModel: Codable {
+    let id: Int?
+    let name, airDate, episode: String?
+    let characters: [String]?
+    let url: String?
+    let created: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case id, name
+        case airDate = "air_date"
+        case episode, characters, url, created
+    }
+}
