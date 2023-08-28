dbfd15fb0a30aacb6d817d9ded121c9f52bac42d
Pavel <Klopat>
2023-08-28 13:35:31 +0300

    CharacterOriginCollectionCell code style fix

--- RickAndMorty/RickAndMorty/CharacterInfo/Cells/CharacterOriginCollectionCell.swift
+++ RickAndMorty/RickAndMorty/CharacterInfo/Cells/CharacterOriginCollectionCell.swift
@@ -11,18 +11,14 @@ import Foundation
 private enum Constants {
     static var cellCornerRadius: CGFloat { 16 }
     static var backgroundViewCornerRadius: CGFloat { 10 }
-    static var planetImageBackgroundViewSidePadding: CGFloat { 8 }
+    static var defaultPadding8: CGFloat { 8 }
+    static var defaultPadding16: CGFloat { 16 }
     static var planetImageBackgroundViewWidth: CGFloat { 64 }
-    static var planetImageViewWidth: CGFloat { 22 }
-    static var planetImageViewHeight: CGFloat { 20 }
+    static var defaultSizeValue20: CGFloat { 20 }
+    static var defaultSizeValue22: CGFloat { 22 }
 
     static var planetImageName: String { "planet" }
     static var planetTypePlaceholderText: String { "Planet" }
-    
-    static var planetNameLabelTopPadding: CGFloat { 16 }
-    static var planetNameLabelLeftPadding: CGFloat { 16 }
-    static var planetNameLabelRightPadding: CGFloat { 8 }
-    static var planetNameLabelHeight: CGFloat { 22 }
 
     static var placeholderDefaultTextColor: UIColor { UIColor().hexStringToUIColor(hex: "#191C2A") }
     static var planetNameTextColor: UIColor { .white }
@@ -73,51 +69,46 @@ class CharacterOriginCollectionCell: UICollectionViewCell {
         label.font = Constants.planetTypeFontStyle
         return label
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: .zero)
         backgroundColor = UIColor().cellBackgroundColor
         layer.cornerRadius = Constants.cellCornerRadius
-    
+
         addSubview(planetImageBackgroundView)
         addSubview(planetNameLabel)
         addSubview(planetTypeLabel)
         planetImageBackgroundView.addSubview(planetImageView)
 
-        planetImageBackgroundView.topAnchor.constraint(
-            equalTo: topAnchor,
-            constant: Constants.planetImageBackgroundViewSidePadding).isActive = true
-        planetImageBackgroundView.leadingAnchor.constraint(
-            equalTo: leadingAnchor,
-            constant: Constants.planetImageBackgroundViewSidePadding).isActive = true
-        planetImageBackgroundView.bottomAnchor.constraint(
-            equalTo: bottomAnchor,
-            constant: -Constants.planetImageBackgroundViewSidePadding).isActive = true
+        planetImageBackgroundView.topAnchor.constraint(equalTo: topAnchor,
+                                                       constant: Constants.defaultPadding8).isActive = true
+        planetImageBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                           constant: Constants.defaultPadding8).isActive = true
+        planetImageBackgroundView.bottomAnchor.constraint(equalTo: bottomAnchor,
+                                                          constant: -Constants.defaultPadding8).isActive = true
         planetImageBackgroundView.widthAnchor.constraint(
             equalToConstant: Constants.planetImageBackgroundViewWidth).isActive = true
-        
+
         planetImageView.centerXAnchor.constraint(equalTo: planetImageBackgroundView.centerXAnchor).isActive = true
         planetImageView.centerYAnchor.constraint(equalTo: planetImageBackgroundView.centerYAnchor).isActive = true
-        planetImageView.widthAnchor.constraint(equalToConstant: Constants.planetImageViewWidth).isActive = true
-        planetImageView.heightAnchor.constraint(equalToConstant: Constants.planetImageViewHeight).isActive = true
-        
-        planetNameLabel.topAnchor.constraint(
-            equalTo: topAnchor,
-            constant: Constants.planetNameLabelTopPadding).isActive = true
-        planetNameLabel.leadingAnchor.constraint(
-            equalTo: planetImageBackgroundView.trailingAnchor,
-            constant: Constants.planetNameLabelLeftPadding).isActive = true
-        planetNameLabel.trailingAnchor.constraint(
-            equalTo: trailingAnchor,
-            constant: -Constants.planetNameLabelRightPadding).isActive = true
-        planetNameLabel.heightAnchor.constraint(equalToConstant: Constants.planetNameLabelHeight).isActive = true
-        
-        planetTypeLabel.topAnchor.constraint(equalTo: planetNameLabel.bottomAnchor, constant: Constants.planetNameLabelRightPadding).isActive = true
-        planetTypeLabel.leadingAnchor.constraint(
-            equalTo: planetImageBackgroundView.trailingAnchor,
-            constant: Constants.planetNameLabelLeftPadding).isActive = true
-        planetTypeLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.planetNameLabelRightPadding).isActive = true
-        planetTypeLabel.heightAnchor.constraint(equalToConstant: Constants.planetNameLabelHeight).isActive = true
+        planetImageView.widthAnchor.constraint(equalToConstant: Constants.defaultSizeValue22).isActive = true
+        planetImageView.heightAnchor.constraint(equalToConstant: Constants.defaultSizeValue20).isActive = true
+
+        planetNameLabel.topAnchor.constraint(equalTo: topAnchor,
+                                             constant: Constants.defaultPadding16).isActive = true
+        planetNameLabel.leadingAnchor.constraint(equalTo: planetImageBackgroundView.trailingAnchor,
+                                                 constant: Constants.defaultPadding16).isActive = true
+        planetNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                  constant: -Constants.defaultPadding8).isActive = true
+        planetNameLabel.heightAnchor.constraint(equalToConstant: Constants.defaultSizeValue22).isActive = true
+
+        planetTypeLabel.topAnchor.constraint(equalTo: planetNameLabel.bottomAnchor,
+                                             constant: Constants.defaultPadding8).isActive = true
+        planetTypeLabel.leadingAnchor.constraint(equalTo: planetImageBackgroundView.trailingAnchor,
+                                                 constant: Constants.defaultPadding16).isActive = true
+        planetTypeLabel.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                  constant: -Constants.defaultPadding8).isActive = true
+        planetTypeLabel.heightAnchor.constraint(equalToConstant: Constants.defaultSizeValue22).isActive = true
     }
 
     required init?(coder: NSCoder) {
